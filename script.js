9e54e7b81809a43a5dc5d6f7c4b857f7e9a5c14f
Gabrielle Guarino <gguarino@umich.edu>
2016-11-20 22:19:00 -0500

    progress report #1

---
+++ script.js
@@ -0,0 +1,20 @@
+var app = angular.module('project351', ['ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'blank.html'
+		})
+		.when('/report1', {
+			templateUrl: 'report1.html'
+		})
+		.when('/report2', {
+			templateUrl: 'report2.html'
+		})
+		.otherwise( {
+			redirectTo: '/home'
+		});
+}])
+
+app.controller('main-controller', ['$scope', function($scope){
+}]);
+
