3a48b079503c49ab7ce5dc3e1e1cc6ebb6a1eb26
Gabrielle Guarino <gguarino@umich.edu>
2016-12-12 03:25:06 -0500

    lil things
    push

--- script.js
+++ script.js
@@ -8,119 +8,21 @@ app.config(['$routeProvider', function($routeProvider) {
 		.when('/report1', {
 			templateUrl: 'report1.html'
 		})
-		.when('/report2', {						
+		.when('/report2', {
 			templateUrl: 'report2.html'
 		})
 		.when('/project',{
 			templateUrl: 'project.html'
 		})
-		.when('/presentation', {
-			templateUrl: 'presentation.html'
-		})
 		.when('/code', {
 			templateUrl: 'code.html'
-		})
-		.when('/info', {
-			templateUrl: 'info.html'
 		})
 		.otherwise( {
 			redirectTo: '/home'
 		});
+}])
+
+app.controller('main-controller', ['$scope', function($scope){
 }]);
-
-app.factory('requesting', function($http) {
-		
-	var data = function(img_link, max_height, max_width, num_colors, fabric_count) {
-		return $http({
-			method: 'GET',
-			url: 'project.py',
-			params: {
-				img_link: img_link,
-				height: max_height,
-				width: max_width,
-				num_colors: num_colors,
-				fabric_count: fabric_count
-			}
-		});
-	};
-
-	return { data: data}
-});
-
-
-app.controller('main-controller', ['$scope', '$http', 'requesting', function($scope, $http, requesting){
-	$scope.submit = function() {
-		$scope.img_link = document.getElementById('image-link').value;
-		$scope.max_height = document.getElementById('max-height').value;
-		$scope.max_width = document.getElementById('max-width').value;
-		$scope.fabric_count = document.getElementById('fabric-count').value;
-		$scope.num_colors = document.getElementById('num-colors').value;
-		var error_found = 0;
-		var error_print = "Error(s):\n\n";
-		var img_check = $scope.img_link.substring(0, 10);
-		if (img_check != 'http://www') {
-			error_print += "Your image link is invalid\n";
-			error_print += "Please put it in the form http://www\n\n";
-			error_found = 1;
-		}
-		if (isNaN($scope.max_height)) {
-			error_print += "Your max height is not a number\n\n";
-			error_found = 1;
-		} 
-		if ($scope.max_height <= 0 || $scope.max_height > 20) {
-			error_print += "Your max height is not in desired range: 1 - 20\n\n";
-			error_found = 1;
-		} 	
-		if (isNaN($scope.max_width)) {
-			error_print += "Your max width is not a number\n\n";
-			error_found = 1;
-		} 
-		if ($scope.max_width <= 0 || $scope.max_width > 20) {
-			error_print += "Your max width is not in desired range: 1 - 20\n\n";
-			error_found = 1;
-		} 
-		if (isNaN($scope.fabric_count)) {
-			error_print += "Your fabric count is not a number\n\n";
-			error_found = 1;
-		} 
-		if ($scope.fabric_count > 20 || $scope.fabric_count <= 0) {
-			error_print += "Your fabric count is not in desired range: 1 - 20\n";
-			error_print += "Some typical fabric counts are 8, 11, 12, 14, 16, 18, 20\n\n"
-			error_found = 1;
-		}
-		if (isNaN($scope.num_colors)) {
-			error_print += "Your number of colors is not a number\n\n";
-			error_found = 1;
-		} 
-		if ($scope.num_colors > 15 || $scope.num_colors <= 0) {
-			error_print += "Your number of colors is not in desired range: 1 - 20\n\n";
-			error_found = 1;
-		} 
-		if (error_found) {
-			alert(error_print);
-		} else {
-			document.getElementById("loading-gif").style.visibility = 'visible';
-			$scope.my_function();
-		}
-	}
-
-
-	$scope.my_function = function() {
-		var recieved_data = requesting.data($scope.img_link, $scope.max_height, $scope.max_width, $scope.num_colors, $scope.fabric_count);
-		
-		recieved_data.then(function(result){
-			console.log(result.data);
-		}, 	function() {
-			console.log("didn't get data");
-		});
-	};
-}]);
-
-
-
-
-
-
-
 
 
